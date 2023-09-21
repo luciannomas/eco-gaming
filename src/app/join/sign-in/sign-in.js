f7e49fc1cdb6d875b57b5c4186fe59086e32f560
luciano <luciano.mastran@gmail.com>
2023-09-21 18:12:24 -0300

    fuction login created

--- src/app/join/sign-in/sign-in.js
+++ src/app/join/sign-in/sign-in.js
@@ -1,3 +1,4 @@
+import { LoginForm } from "@/components/Auth/LoginForm";
 import Link from "next/link";
 import { JoinLayout } from "../../../layouts/JoinLayout/page";
 import styles from "./sign-in.module.scss";
@@ -10,7 +11,7 @@ export default function SignInPage() {
       <JoinLayout>
         <div>
           <h3>Iniciar sesión</h3>
-
+          <LoginForm />
 
           <div className={styles.actions}>
             <Link href="/join/sign-up">¿No tienes una cuenta?</Link>
---
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -0,0 +1,73 @@
+import { Form } from "semantic-ui-react";
+import { useFormik } from "formik";
+import { useRouter } from "next/navigation";
+import { initialValues, validationSchema } from "./LoginForm.form";
+import { ENV } from "../../../utils/index";
+
+const login = async(data) => {
+  try {
+    const url = `${ENV.API_URL}/${ENV.ENDPOINTS.AUTH.LOGIN}`;
+    const params = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, params);
+    const result = await response.json();
+
+    if (response.status !== 200) throw result;
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export function LoginForm() {
+  const router = useRouter();
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      try {
+        const response = await login(formValue);
+        // login(response.jwt);
+        console.log(response)
+
+        router.push("/");
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
+
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="identifier"
+        type="text"
+        placeholder="Correo electronico o nombre de usuario"
+        value={formik.values.identifier}
+        onChange={formik.handleChange}
+        error={formik.errors.identifier}
+      />
+      <Form.Input
+        name="password"
+        type="password"
+        placeholder="Contraseña"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        error={formik.errors.password}
+      />
+
+      <Form.Button type="submit" fluid loading={formik.isSubmitting}>
+        Entrar
+      </Form.Button>
+    </Form>
+  );
+}
